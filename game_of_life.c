ca6ec7906ff94e3e6befd278aa1b8f0763193ad6
krainyukov <a.a.kraynyukov@gmail.com>
2024-02-23 02:40:50 +0300

    first try to make menu

--- game_of_life.c
+++ game_of_life.c
@@ -2,16 +2,17 @@
 #include <string.h>
 #include <stdlib.h>
 #include<time.h>
-#define WIDTH 190
+#define WIDTH 90
 #define HEIGHT 55
 
 void init_game();
-void draw_hello();
+//void draw_hello(char* hello_frase, int len_hello_frase);
 void print_frame(int grid[HEIGHT][WIDTH]);
 void fill_with_random(int grid[HEIGHT][WIDTH]);
 int game_logic(int grid[HEIGHT][WIDTH], int grid1[HEIGHT][WIDTH]);
 int count(int grid[HEIGHT][WIDTH], int i, int j);
-
+void refresh_grid(int grid[HEIGHT][WIDTH], int grid1[HEIGHT][WIDTH]);
+void draw_menu(char* hello_frase, int len_hello_frase, int* option);
 int main() {
     initscr();
     init_game();
@@ -20,10 +21,12 @@ int main() {
     return 0;
 }
 
+/* This function creates grids, current grid and future grid. The current
+one is filled according to the selected option in the menu. Either 
+randomly or as set with the cursor. Then there is an evolution process 
+inside the loop waiting for q to exit. */
+
 void init_game() {
-    // int size = sizeof(int*) * HEIGHT + sizeof(int) * HEIGHT * WIDTH;
-    // int **grid1 = (int**)malloc(size);
-    // int* first_element = (int*)(grid1 + HEIGHT);
     int grid[HEIGHT][WIDTH];
     int grid1[HEIGHT][WIDTH];
     for (int i = 0; i < HEIGHT; i++) {
@@ -31,30 +34,35 @@ void init_game() {
             grid1[i][j] = 0;
         }
     }
-    fill_with_random(grid);
+
+    
     char* hello_frase = "GAME OF LIFE";
     int len_hello_frase = strlen(hello_frase);
-    draw_hello(hello_frase, len_hello_frase);
+    //draw_hello(hello_frase, len_hello_frase);
+    int option = 1;
+    draw_menu(hello_frase, len_hello_frase, &option);
     char keyboard_command;
     int generation = 0;
+
+    fill_with_random(grid);
     while ((keyboard_command = getch()) != 'q') {
         clear();
         if (generation == 0) {
             print_frame(grid);
         } else {
             game_logic(grid, grid1);
-            for (int i = 0; i < HEIGHT; i++) {
-                for (int j = 0; j < WIDTH; j++) {
-                    grid[i][j] = grid1[i][j];
-                }
-            }
+            refresh_grid(grid, grid1);
             print_frame(grid);
             
         }
         generation++;
-        printw("%d", generation);
+        printw("FRAME: %d", generation);
     }
 }
+
+/* In this function, the neighbors are counted using the count function 
+and, depending on whether the current cell is alive or dead, conclusions 
+about its future are drawn. */
 
 int game_logic(int grid[HEIGHT][WIDTH], int grid1[HEIGHT][WIDTH]) {
     for (int i = 0; i < HEIGHT; i++) {
@@ -97,17 +105,55 @@ int count(int grid[HEIGHT][WIDTH], int i, int j) {
     return count;
 }
 
-void draw_hello(char* hello_frase, int len_hello_frase) {
-    char* press_any = "press any key to start or <q> for escape";
-    int len_press_any = strlen(press_any);
+/* Initial screen */
+
+// void draw_hello(char* hello_frase, int len_hello_frase) {
+//     char* press_any = "press any key to start or <q> for escape";
+//     int len_press_any = strlen(press_any);
+//     for (int i = 0; i < HEIGHT; i++) {
+//         for (int j = 0; j < WIDTH; j++) {
+//             if (i == HEIGHT / 2 && j == (WIDTH - len_hello_frase) / 2) {
+//                 printw("%s", hello_frase);
+//                 j = j + len_hello_frase - 1;
+//             } else if (i == HEIGHT / 2 + 3 && j == (WIDTH - len_press_any) / 2) {
+//                 printw("%s", press_any);
+//                 j = j + len_press_any - 1;
+//             } else {
+//                 printw(" ");
+//             }
+//         }
+//         printw("\n");
+//     }
+// }
+
+void draw_menu(char* hello_frase, int len_hello_frase, int* option) {
+    int star = 0;
+    if (*option == 1) {
+        star = HEIGHT / 2 + 3;
+    } else {
+        star =  HEIGHT / 2 + 5;
+    }
+    char* option1 = "random game";
+    char* option2 = "set new game";
+    int len_option1 = strlen(option1);
+    int len_option2 = strlen(option2);
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             if (i == HEIGHT / 2 && j == (WIDTH - len_hello_frase) / 2) {
                 printw("%s", hello_frase);
                 j = j + len_hello_frase - 1;
-            } else if (i == HEIGHT / 2 + 3 && j == (WIDTH - len_press_any) / 2) {
-                printw("%s", press_any);
-                j = j + len_press_any - 1;
+            } else if (i == HEIGHT / 2 + 3 && j == (WIDTH - len_option1) / 2) {
+                printw("%s", option1);
+                j = j + len_option1 - 1;
+            } else if (i == HEIGHT / 2 + 5 && j == (WIDTH - len_option2) / 2) {
+                printw("%s", option2);
+                j = j + len_option2 - 1;
+            } else if (*option == 1 && i == star && j == (WIDTH - len_option1) / 2 - 1) {
+                printw("*%s",option1);
+                j = j + len_option1;
+            } else if (*option == 2 && i == star && j == (WIDTH - len_option2) / 2 - 1) {
+                printw("*%s", option2);
+                j = j + len_option2;
             } else {
                 printw(" ");
             }
@@ -138,3 +184,10 @@ void fill_with_random(int grid[HEIGHT][WIDTH]) {
     }
 }
 
+void refresh_grid(int grid[HEIGHT][WIDTH], int grid1[HEIGHT][WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            grid[i][j] = grid1[i][j];
+        }
+    }
+}
